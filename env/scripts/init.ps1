b768e1ece94af434073c0048f1ca7abca767a865
annaelde <elde.av@gmail.com>
2017-10-14 21:21:02 -0400

    set working directory as project root now.

--- env/scripts/init.ps1
+++ env/scripts/init.ps1
@@ -1,8 +1,10 @@
-# IMPORTANT: Only run the first time you're setting up your project!
-# I mean, you could run it more times... but it would be pointless.
+# Script to initialize Django development environment
+
+# Set the working directory
+Set-Location (Get-Item (Split-Path -Parent $MyInvocation.MyCommand.Path)).Parent.Parent.FullName
 
 # Get the environmental variable from the Docker .env file
-Get-Content ../.env | Foreach-Object{
+Get-Content ./env/.env | Foreach-Object{
     If (-NOT ($_ -eq "")){
         $var = $_.Split('=')
         New-Variable -Name $var[0] -Value $var[1]
@@ -12,17 +14,14 @@ Get-Content ../.env | Foreach-Object{
     }
 }
 
-# Set the working directory
-Set-Location ../../
-
 # Create Python virtual env
-python -m venv env
+python -m venv ./env/py
 
 # Activate the virtual environment
-./env/bin/Activate.ps1
+./env/py/bin/Activate.ps1
 
 # Install requirements
-pip install -r env/requirements.pip
+pip install -r ./config/dev/requirements.pip
 
 # Create directories for Django website
 New-Item ./site -Type directory
@@ -34,7 +33,7 @@ New-Item ./assets/static -Type directory
 django-admin startproject $project ./site
 
 # Run docker-compose
-docker-compose up -d -f ./docker/docker-compose.yml
+docker-compose up -d -f ./env/docker-compose.yml
 
 # Activate Gunicorn
 gunicorn --bind 127.0.0.1:8080 $project.wsgi
