be751d573b556c7b914525fcf970f65da44c6644
annaelde <elde.av@gmail.com>
2017-10-22 23:02:02 -0400

    add post detail view.

--- site/threads/urls.py
+++ site/threads/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list.as_view()),
+    url(r'/?P<id>[0-9]/$', views.post_detail.as_view())
 ]
