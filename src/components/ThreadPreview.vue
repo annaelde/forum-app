157b577774103a3060ad1b665d51cc6a2541a6d0
annaelde <elde.av@gmail.com>
2017-10-23 08:03:44 -0400

    add action buttons and router links.

--- src/components/ThreadPreview.vue
+++ src/components/ThreadPreview.vue
@@ -2,19 +2,36 @@
     <div class="box">
         <div class="columns">
             <div class="column is-three-quarters">
-                <h1 class="title">{{ thread.title }}</h1>
+                <router-link :to="'/thread/' + thread.id + '/' + thread.slug">
+                    <h1 class="title">{{ thread.title }}</h1>
+                </router-link>
                 <h2 class="subtitle">Posted by {{ thread.author }} on {{ thread.created }}</h2>
             </div>
             <div class="column is-clearfix">
-                <button @click="open = !open" class="button is-pulled-right">
-                    <span class="icon">
-                        <i class="fa" v-bind:class="{'fa-plus' : !open, 'fa-minus' : open}">
-                            <span class="is-accessible" v-if="!open">Open</span>
-                            <span class="is-accessible" v-else>Close</span>
-                        </i>
-                    </span>
-
-                </button>
+                <div class="is-pulled-right">
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fa fa-bookmark">
+                                <span class="is-accessible">Bookmark</span>
+                            </i>
+                        </span>
+                    </button>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fa fa-link">
+                                <span class="is-accessible">Copy Link</span>
+                            </i>
+                        </span>
+                    </button>
+                    <button @click="open = !open" class="button">
+                        <span class="icon">
+                            <i class="fa" v-bind:class="{'fa-plus' : !open, 'fa-minus' : open}">
+                                <span class="is-accessible" v-if="!open">Open</span>
+                                <span class="is-accessible" v-else>Close</span>
+                            </i>
+                        </span>
+                    </button>
+                </div>
             </div>
         </div>
         <div v-bind:class="{'is-hidden' : !open}" class="content">
@@ -41,10 +58,3 @@ export default Vue.component('thread-preview', {
     }
 })
 </script>
-
-
-<style lang="sass">
-.is-accessible
-    font-size: 0
-    
-</style>
