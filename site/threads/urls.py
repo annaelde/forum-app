a4585dc9a0d30b223db14755a00df79b96dd1f28
annaelde <elde.av@gmail.com>
2017-10-23 00:41:14 -0400

    Remove leading slash from URL.

--- site/threads/urls.py
+++ site/threads/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list.as_view()),
-    url(r'/?P<id>[0-9]/$', views.post_detail.as_view())
+    url(r'P<id>[0-9]/$', views.post_detail.as_view())
 ]
