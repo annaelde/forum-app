415203947fe5a9e87e78e1e22c92bcb7b031dc13
annaelde <elde.av@gmail.com>
2017-10-22 22:57:51 -0400

    tweak post serializer and add user serializer.

--- site/threads/serializers.py
+++ site/threads/serializers.py
@@ -1,9 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import User
+from rest_framework import serializers
 
 from .models import Post
 
 
-class PostSerializer(ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+    # Hash mark used to remove padding--might need to be - in Linux instead
+    created = serializers.DateTimeField(format='%m/%d/%y at %#I:%M%p')
+    author = serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = Post
         fields = ('title', 'author', 'content', 'created')
+
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, queryset=Post.objects.all())
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'posts')
