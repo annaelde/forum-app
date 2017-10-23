0c8d013ac20ee210ab1985b11065888f46e98cf8
annaelde <elde.av@gmail.com>
2017-10-22 22:54:24 -0400

    update to use generic API views.

--- site/threads/views.py
+++ site/threads/views.py
@@ -5,18 +5,13 @@ from rest_framework.renderers import JSONRenderer
 
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny
 
+class post_list(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
 
-# Create your views here.
-def post_list(request):
-    if request.method == 'GET':
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class post_detail(RetrieveAPIView):
+    model = Post
