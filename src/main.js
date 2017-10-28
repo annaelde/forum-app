c7cefa3401730ec1ce77abd15f824cda2690b273
annaelde <elde.av@gmail.com>
2017-10-28 11:07:55 -0400

    move router into its own file and add store and filters.

--- src/main.js
+++ src/main.js
@@ -1,25 +1,10 @@
-import Router from 'vue-router'
 import App from './App.vue'
-import Board from './components/Board.vue'
-import Thread from './components/Thread.vue'
+import store from './store'
+import router from './router'
+import filters from './plugins/filters'
 
-// Router Config
-Vue.use(Router)
-
-const routes = [
-    { path: '/', component: Board },
-    { 
-        path: '/thread/:id/:slug/',
-        component: Thread
-    }
-]
-
-const router = new Router({
-    routes
-})
-
-// App Config
 var app = new Vue({
     router,
+    store,
     render: createEle => createEle(App)
 }).$mount('#app')
