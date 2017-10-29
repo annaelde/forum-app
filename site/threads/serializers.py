7b40c2ad569405808e438cb0df42923cfe05fc39
annaelde <elde.av@gmail.com>
2017-10-29 03:23:02 -0400

    add board name as field.

--- site/threads/serializers.py
+++ site/threads/serializers.py
@@ -6,7 +6,9 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     created = serializers.DateTimeField()
     author = serializers.ReadOnlyField(source='author.username')
+    board = serializers.ReadOnlyField(source='board.name')
 
     class Meta:
         model = Post
-        fields = ('title', 'author', 'content', 'created', 'id', 'slug')
+        fields = ('title', 'author', 'content',
+                  'created', 'id', 'slug', 'board')
