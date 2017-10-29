aaca6fac2849a75e1f46f1a086f596e71327f312
annaelde <elde.av@gmail.com>
2017-10-29 08:19:39 -0400

    add sidebar component.

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section column">
+      <h1>{{ board.name }}</h1>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default Vue.component('sidebar', {
+    name: 'sidebar',
+    computed: mapState({
+        board: state => state.board.data,
+        error: state => state.board.error
+    })
+})
+</script>
+
+
+<style lang="sass">
+
+</style>
