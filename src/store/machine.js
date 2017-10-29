c013a81ef1891a033a491ad70355c386ede15d89
annaelde <elde.av@gmail.com>
2017-10-29 08:14:57 -0400

    add state machine.

---
+++ src/store/machine.js
@@ -0,0 +1,36 @@
+function StateMachine() {
+    return {
+        state: 'idle',
+        do: function(action) {
+            if (this.behavior[this.state][action]) this.state = this.behavior[this.state][action]
+            else throw 'Action cannot be performed with this state.'
+        },
+        behavior: {
+            idle: {
+                // Default state before interaction
+                request: 'loading'
+            },
+            loading: {
+                // Sending a request to the server
+                // Resolve/reject immediately reset the machine
+                resolve: 'idle',
+                reject: 'idle',
+                // Update and handle used for more complicated behaviors
+                update: 'updating',
+                handle: 'handling'
+            },
+            updating: {
+                // Updating based on response
+                request: 'loading',
+                complete: 'idle'
+            },
+            handling: {
+                // Handling an error
+                request: 'loading', // Re-request another resource
+                throw: 'idle' // Reset to idle
+            }
+        }
+    }
+}
+
+export default StateMachine
