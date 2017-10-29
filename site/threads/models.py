0e273d16e6215d978078b830119113f4bc864bb5
annaelde <elde.av@gmail.com>
2017-10-29 03:22:51 -0400

    add fields/methods to integrate board app.

--- site/threads/models.py
+++ site/threads/models.py
@@ -1,21 +1,27 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from django.db import models
+
+from boards.models import Board
+
 
 def get_null_user():
     return get_user_model().objects.get_or_create(username='deleted')
+
+
+def get_null_board():
+    return Board.objects.get_or_create(name='deleted')
 
 
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=128)
     content = models.TextField(max_length=1024)
-    author = models.ForeignKey(User, related_name='posts',
+    author = models.ForeignKey(get_user_model(), related_name='posts',
                                on_delete=models.SET(get_null_user))
+    board = models.ForeignKey(Board, related_name='posts', 
+                              on_delete=models.SET(get_null_board))
     slug = models.SlugField()
-
+    key = models.CharField(max_length=6)
+    
     def __str__(self):
-        if self.title is not None:
-            return self.title
-        else:
-            return "New Post"
+        return self.title
