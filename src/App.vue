30f7910cf8ac7f2ea0718d65683a7cfcbe2688af
annaelde <elde.av@gmail.com>
2017-10-29 08:15:36 -0400

    add state machine, error component, and loading overlay.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
     <div>
-        <navbar v-bind:auth="auth"></navbar>
-        <router-view></router-view>
+        <navigation></navigation>
+        <div v-if="current === 'idle'" class="columns">
+            <router-view name="main"></router-view>
+            <router-view name="sidebar"></router-view>
+        </div>
+        <transition name="fade">
+            <main v-if="current === 'loading'">
+                <div class="overlay">
+                    <span class="icon is-large"><i  class="fa fa-circle-o-notch fa-spin fa-3x"></i></span>
+                </div>
+            </main>
+        </transition>
+        <transition name="balloon">
+            <main v-if="current === 'handling'" class="section">
+                <error :response="error"></error>
+            </main>
+        </transition>
+        <footnotes></footnotes>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import Navbar from './components/Navbar.vue'
+import Navigation from './components/Navigation.vue'
+import Footnotes from './components/Footnotes.vue'
 
 export default {
     name: 'app',
     computed: mapState({
-        auth: 'token'
+        error: state => state.error,
+        current: state => state.machine.state
     })
 }
 </script>
@@ -23,6 +41,22 @@ export default {
 @import "~bulma/bulma"
 @import "~bulmaswatch/darkly/_overrides.scss"
 @import "styles/main"
+
+.overlay
+    position: fixed
+    z-index: 3
+    height: 100%
+    width: 100%
+    background: rgba(0, 0, 0, 0.3)
+    top: 0
+    left: 0
+
+.overlay .icon
+    position: absolute
+    top: calc(50% - 5vh)
+    left: calc(50% - 5vh)
+    font-size: 10vh
+
 </style>
 
 <style lang="scss">
