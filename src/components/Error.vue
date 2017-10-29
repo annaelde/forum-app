6d391b5206f753d758a657a47befe94b9d21609b
annaelde <elde.av@gmail.com>
2017-10-29 08:15:56 -0400

    add reusable error component.

---
+++ src/components/Error.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="hero box is-danger is-bold">
+        <div class="hero-body">
+            <h1 class="title">
+                {{ response.status }} {{ response.statusText }}
+            </h1>
+            <h2 class="subtitle">
+                {{ customMessage() }}
+            </h2>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default Vue.component('error', {
+    name: 'error',
+    props: [
+        'response'
+    ],
+    methods: {
+        customMessage: function() {
+            var message = `Oops, there's been an error loading this page. Try loading it again.`
+            switch(this.response.status)
+            {
+                case 500: 
+                    message = `Forum couldn't process this request.`
+                    break
+                case 502: 
+                    message = `Forum couldn't connect to the server.` 
+                    break
+                case 503: 
+                    message = `The server is overloaded. Try connecting again.`
+                    break                    
+                case 403: 
+                    message = `You don't have permission to access this. Are you logged in?`
+                    break                    
+                case 404: 
+                    message = `This page doesn't exist.`
+                    break
+            }
+            return message
+        }
+    }
+})
+</script>
+
+
+<style lang="sass">
+
+</style>
