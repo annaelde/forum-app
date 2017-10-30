a38ab463f67f10c0e48be198b47a834beee6eafc
annaelde <elde.av@gmail.com>
2017-10-30 03:19:47 -0400

    setup test for post model.

--- site/threads/tests.py
+++ site/threads/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from boards.models import Board
+
+from .models import Post
+
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.author = get_user_model().objects.create_user(username='tester',
+                                                           email='tester@forum.com',
+                                                           password='ilovetesting')
+        self.board = Board.objects.create(name='testBoard',
+                                          description='Where the best tests happen.')
+        self.post = Post.objects.create(title='VueJS Isn\'t Great--It\'s Amazing!',
+                                        author=self.author,
+                                        content='VueJS is an amazing framework. Wow!',
+                                        board=self.board)
+
+    def test_slug(self):
+        self.assertEqual(self.post.slug, 'vuejs-isnt-great-its-amazing')
