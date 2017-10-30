408672186a8a47091f3bec997ddd8394ecab38dc
annaelde <elde.av@gmail.com>
2017-10-30 09:37:10 -0400

    add MultipleFieldLookupMixin to post detail GET view.

--- site/threads/views.py
+++ site/threads/views.py
@@ -1,19 +1,23 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.parsers import JSONParser
+from rest_framework.permissions import AllowAny
 from rest_framework.renderers import JSONRenderer
+
+from utils.mixins import MultipleFieldLookupMixin
 
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.permissions import AllowAny
+
 
 class post_list(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (AllowAny,)
 
-class post_detail(RetrieveAPIView):
+class post_detail(MultipleFieldLookupMixin, RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    lookup_fields = ('key', 'slug')
     model = Post
---
+++ site/utils/mixins.py
@@ -0,0 +1,20 @@
+from django.shortcuts import get_object_or_404
+
+
+class MultipleFieldLookupMixin(object):
+    """
+    Apply this mixin to any view or viewset to get multiple field filtering
+    based on a `lookup_fields` attribute, instead of the default single field filtering.
+    Source: Django REST Framework Documentation
+    """
+
+    def get_object(self):
+        queryset = self.get_queryset()             # Get the base queryset
+        queryset = self.filter_queryset(queryset)  # Apply any filter backends
+        filter = {}
+        for field in self.lookup_fields:
+            if self.kwargs[field]:  # Ignore empty fields.
+                filter[field] = self.kwargs[field]
+        obj = get_object_or_404(queryset, **filter)  # Lookup the object
+        self.check_object_permissions(self.request, obj)
+        return obj
