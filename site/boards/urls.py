ad9604d491b497dea50d98dd81fcad5728609698
annaelde <elde.av@gmail.com>
2017-10-30 11:57:21 -0400

    fix regex for board slug.

--- site/boards/urls.py
+++ site/boards/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import url, include
-from . import views
+from django.conf.urls import include, url
+
 from threads.urls import urlpatterns as thread_patterns
+
+from .views import board_detail, board_list
 
 urlpatterns = [
     # ex: /api/boards/
-    url(r'^$', views.board_list.as_view()),
-    # ex: /api/boards/{name}/
-    url(r'^(?P<board>[a-zA-Z0-9])/$', views.board_detail.as_view()),
-    # ex: /api/boards/{name}/threads/
-    url(r'^(?P<board>[a-zA-Z0-9])/threads/', include(thread_patterns))
+    url(r'^$', board_list.as_view()),
+    # ex: /api/boards/{slug}/
+    url(r'^(?P<slug>[a-zA-Z0-9]+?)/$', board_detail.as_view()),
+    # ex: /api/boards/{slug}/threads/
+    url(r'^(?P<slug>[a-zA-Z0-9]+?)/threads/', include(thread_patterns))
 ]
