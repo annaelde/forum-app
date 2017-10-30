a300f6d87e0e97dcd310945f3ea40cd55698c8a6
annaelde <elde.av@gmail.com>
2017-10-30 07:06:51 -0400

    add unit test for user model.

--- site/users/tests.py
+++ site/users/tests.py
@@ -1,3 +1,33 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class UserTest(TestCase):
+    def test_username_regex(self):
+        failingUsernames = ['..test', '__test', '--test',
+                            'mySuperLongUsernameThatShouldFail', 'tes']
+        passingUsernames = ['.test', '-test', '_test']
+
+        for name in failingUsernames:
+            user = User(username=name,
+                        password='testingpassword',
+                        email='email@test.com')
+            with self.assertRaises(ValidationError, msg=name):
+                if user.full_clean():
+                    user.save()
+            count = User.objects.filter(username=name).count()
+            self.assertEqual(count, 0, name)
+
+        for name in passingUsernames:
+            user = User(username=name,
+                        password='testingpassword',
+                        email='email@test.com')
+            try:
+                user.full_clean()
+                user.save()
+            except:
+                self.fail(msg='Validation error for ' + name)
+            count = User.objects.filter(username=name).count()
+            self.assertEqual(count, 1, name)
