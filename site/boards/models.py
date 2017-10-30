0a469dba261f63b08f9d07b8fe5eee82dc18a22d
annaelde <elde.av@gmail.com>
2017-10-30 09:34:01 -0400

    add slug generation.

--- site/boards/models.py
+++ site/boards/models.py
@@ -1,10 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from slugify import slugify
+
+
+def slugify_board_name(name: str) -> str:
+    slug = name.replace('&', 'and')
+    return slugify(slug, max_length=64, separator='',
+                   save_order=True, entities=False)
+
 
 class Board(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
     name = models.CharField(max_length=64, unique=True, blank=False)
     description = models.CharField(max_length=712, blank=False)
-    slug = models.SlugField()
+    slug = models.SlugField(db_index=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_board_name(slug)
+        super(Board, self).save(*args, **kwargs)
