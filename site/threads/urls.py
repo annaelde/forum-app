b65db6dffe5cbe42510b2ee1b2ee993c39fb0eab
annaelde <elde.av@gmail.com>
2017-10-30 11:58:15 -0400

    change view names from post to thread.

--- site/threads/urls.py
+++ site/threads/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from . import views
+from .views import thread_detail, thread_list
 
 urlpatterns = [
     # ex: /api/board/{name}/threads/
-    url(r'^$', views.post_list.as_view()),
+    url(r'^$', thread_list.as_view()),
     # ex: /api/board/{name}/threads/{key}/{title}
-    url(r'(?P<key>[a-zA-Z0-9]{4,4})/(?P<slug>[-\w]+?)/$', views.post_detail.as_view())
+    url(r'(?P<key>[a-zA-Z0-9]{4,4})/(?P<slug>[-\w]+?)/$', thread_detail.as_view())
 ]
--- site/threads/views.py
+++ site/threads/views.py
@@ -1,9 +1,5 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.parsers import JSONParser
 from rest_framework.permissions import AllowAny
-from rest_framework.renderers import JSONRenderer
 
 from utils.mixins import MultipleFieldLookupMixin
 
@@ -11,13 +7,15 @@ from .models import Post
 from .serializers import PostSerializer
 
 
-class post_list(ListAPIView):
+class thread_list(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (AllowAny,)
 
-class post_detail(MultipleFieldLookupMixin, RetrieveAPIView):
+
+class thread_detail(MultipleFieldLookupMixin, RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
     lookup_fields = ('key', 'slug')
     model = Post
