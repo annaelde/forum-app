6a6d6726fb8540af2a709f24675f9aedb8b70ce1
annaelde <elde.av@gmail.com>
2017-10-30 09:37:32 -0400

    override user __init__() to add regex validators for username

--- site/users/models.py
+++ site/users/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(*args, **kwargs)
+        self._meta.get_field('username').validators = [RegexValidator(regex=r'^([.\-_]?[a-zA-Z0-9]+)+[.\-_]?$'),
+                                                       RegexValidator(regex=r'^.{4,32}$')]
