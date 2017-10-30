cf2b79235bc1c9d09c057e209c7df602bfd023b6
annaelde <elde.av@gmail.com>
2017-10-30 08:06:00 -0400

    add test for board model.

--- site/boards/tests.py
+++ site/boards/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Board
 
-# Create your tests here.
+
+class BoardTest(TestCase):
+    def test_slugs(self):
+        names = ['Birds & Bees', 'TheWatchtower',
+                 'Cooking', 'We Love Cats!', 'The A As']
+        slugs = ['birdsandbees', 'thewatchtower',
+                 'cooking', 'welovecats', 'theaas']
+
+        for name, slug in zip(names, slugs):
+            board = Board(name=name,
+                          description='test')
+            board.full_clean()
+            board.save()
+            self.assertEquals(board.slug, slug,
+                              msg=f'Expected "{name}" to be slugified to "{slug}" but got "{board.slug}"')
