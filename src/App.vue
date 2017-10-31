978cb2c457e647c3654f3d7abda79d395311ff76
annaelde <elde.av@gmail.com>
2017-10-31 12:27:15 -0400

    add progress bar.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
     <div>
         <navigation></navigation>
+        <div class="progress-container">
+            <transition name="fade-out">
+                <progress v-show="progress != 100" class="progress is-info is-small is-radiusless" :value="progress" max="100"></progress>
+            </transition>
+        </div>
         <div v-show="current === 'idle'" class="columns">
             <router-view name="main"></router-view>
             <router-view name="sidebar"></router-view>
         </div>
-        <transition name="fade">
-            <div v-if="current === 'loading'">
-                <div class="overlay">
-                    <span class="icon is-large">
-                        <i class="fa fa-circle-o-notch fa-spin fa-3x"></i>
-                    </span>
-                </div>
-            </div>
-        </transition>
         <transition name="balloon">
             <div v-if="current === 'handling'" class="section">
                 <error :response="error"></error>
@@ -27,13 +23,53 @@
 import { mapState } from 'vuex'
 import Navigation from './components/layout/Navigation.vue'
 import Footnotes from './components/layout/Footnotes.vue'
+import TWEEN from '@tweenjs/tween.js'
 
 export default {
     name: 'app',
+    data: function() {
+        return {
+            progress: 0
+        }
+    },
     computed: mapState({
         error: state => state.error,
         current: state => state.machine.state
-    })
+    }),
+    watch: {
+        current: function(newState, oldState) {
+            var newValue
+            var oldValue
+
+            if (oldState === 'idle') {
+                oldValue = 0
+                newValue = 50
+            } else if (oldState === 'loading') {
+                oldValue = 50
+                newValue = 100
+            } else if (!oldState) {
+                oldValue = 0
+                newValue = 0
+            }
+
+            var view = this
+
+            function animate() {
+                if (TWEEN.update()) {
+                    requestAnimationFrame(animate)
+                }
+            }
+
+            new TWEEN.Tween({ x: oldValue })
+                .easing(TWEEN.Easing.Quadratic.Out)
+                .to({ x: newValue }, 500)
+                .onUpdate(function(object) {
+                    view.progress = object.x.toFixed(0)
+                })
+                .start()
+            animate()
+        }
+    }
 }
 </script>
 
@@ -44,21 +80,8 @@ export default {
 @import "~bulmaswatch/darkly/_overrides.scss"
 @import "styles/main"
 
-.overlay
-    position: fixed
-    z-index: 3
-    height: 100%
-    width: 100%
-    background: rgba(0, 0, 0, 0.3)
-    top: 0
-    left: 0
-
-.overlay .icon
-    position: absolute
-    top: calc(50% - 5vh)
-    left: calc(50% - 5vh)
-    font-size: 10vh
-
+.progress-container
+    min-height: calc(0.85em + 1.5rem)
 </style>
 
 <style lang="scss">
