7c6638c71051ec55c501996bdc4f38db3e1946d2
annaelde <elde.av@gmail.com>
2017-10-31 05:32:44 -0400

    override queryset for thread list to include board.

--- site/threads/views.py
+++ site/threads/views.py
@@ -8,14 +8,17 @@ from .serializers import PostSerializer
 
 
 class thread_list(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        board = self.kwargs['board']
+        return Post.objects.filter(board__slug=board)
 
 
 class thread_detail(MultipleFieldLookupMixin, RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (AllowAny,)
-    lookup_fields = ('key', 'slug')
+    lookup_fields = ('board', 'key', 'thread')
     model = Post
