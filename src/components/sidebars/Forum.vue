489afc3562e3dad77a1f14c23bf02872deda8eed
annaelde <elde.av@gmail.com>
2017-10-31 12:29:10 -0400

    add icons to stats and remove dispatch call

--- src/components/sidebars/Forum.vue
+++ src/components/sidebars/Forum.vue
@@ -1,24 +1,40 @@
 <template>
-  <section class="section column">
-      <div class="content">
-          <h1 class="title">Welcome to Forum</h1>
-          <h2>Stats</h2>
-          <table>
-              <tr>
-                  <td>Users</td>
-                  <td>{{ stats.users }}</td>
-              </tr>
-              <tr>
-                  <td>Boards</td>
-                  <td>{{ stats.boards }}</td>
-              </tr>
-              <tr>
-                  <td>Threads</td>
-                  <td>{{ stats.threads }}</td>
-              </tr>
-          </table>
-      </div>
-  </section>
+    <section class="section column">
+        <div class="content">
+            <h1 class="title">Welcome to Forum</h1>
+            <p>A new type of message board.</p>
+            <h2>Stats</h2>
+            <table>
+                <tr>
+                    <td>
+                        <span class="icon">
+                            <i class="fa fa-users"></i>
+                        </span>
+                        <span>Users</span>
+                    </td>
+                    <td>{{ stats.users }}</td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="icon">
+                            <i class="fa fa-list-alt"></i>
+                        </span>
+                        <span>Boards</span>
+                    </td>
+                    <td>{{ stats.boards }}</td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="icon">
+                            <i class="fa fa-bookmark"></i>
+                        </span>
+                        <span>Threads</span>
+                    </td>
+                    <td>{{ stats.threads }}</td>
+                </tr>
+            </table>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -29,10 +45,7 @@ export default Vue.component('forum-sidebar', {
     computed: mapState({
         stats: state => state.forum.stats,
         error: state => state.forum.error
-    }),
-    created(){
-        this.$store.dispatch('forum/loadStats')
-    }
+    })
 })
 </script>
 
