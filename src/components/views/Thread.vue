a74912463fadd633667fa27c82629df71d7a3612
annaelde <elde.av@gmail.com>
2017-10-31 12:30:26 -0400

    delete loading spinner and back button.

--- src/components/views/Thread.vue
+++ src/components/views/Thread.vue
@@ -1,37 +1,17 @@
 <template>
-    <div>
-        <nav class="section">
-            <div class="container">
-                <router-link :to='`/~${board.slug}`' class="button">
-                    <span class="icon">
-                        <i class="fa fa-arrow-left">
-                        </i>
-                    </span>
-                    <span>Back to Board</span>
-                </router-link>
-            </div>
-        </nav>
-        <article v-if="loading" class="section thread skeleton">
-            <span class="icon is-large">
-                <i class="fa fa-spinner fa-spin fa-3x"></i>
-            </span>
-        </article>
-        <article v-else-if="!loading && thread">
-            <section class="hero is-info is-bold">
-                <div class="hero-body">
-                    <div class="container">
-                        <h1 class="title">{{ thread.title }}</h1>
-                        <h2 class="subtitle">Posted by {{ thread.author }} on {{ thread.created | timeElapsed }}</h2>
-                    </div>
-                </div>
-            </section>
-            <section class="section">
+    <article class="section column is-three-quarters">
+        <header class="hero is-primary is-round">
+            <div class="hero-body">
                 <div class="container">
-                    {{ thread.content }}
+                    <h1 class="title">{{ thread.title }}</h1>
+                    <h2 class="subtitle">Posted by {{ thread.author }} {{ thread.created | timeElapsed }}</h2>
                 </div>
-            </section>
-        </article>
-    </div>
+            </div>
+        </header>
+        <section class="section">
+            {{ thread.content }}
+        </section>
+    </article>
 </template>
 
 <script>
@@ -43,12 +23,14 @@ export default Vue.component('thread', {
     computed: mapState({
         board: state => state.board.data,
         thread: state => state.thread.data,
-        error: state => state.thread.error,
-        loading: state => state.thread.loading
+        error: state => state.error
     }),
     created() {
-        this.$store.dispatch('thread/loadThread', { board: this.$route.params.board, 
-        key: this.$route.params.key, slug: this.$route.params.slug })
+        this.$store.dispatch('thread/loadThread', {
+            board: this.$route.params.board,
+            key: this.$route.params.key,
+            slug: this.$route.params.slug
+        })
     }
 })
 </script>
