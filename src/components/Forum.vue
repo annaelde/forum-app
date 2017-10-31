eaac1dffb361ceb14244aada2a350e171d2afb5f
annaelde <elde.av@gmail.com>
2017-10-31 04:23:27 -0400

    add grid layout for cards.

--- src/components/Forum.vue
+++ src/components/Forum.vue
@@ -1,26 +1,28 @@
 <template>
-    <main class="section column">
-        <div v-for="board in boards" :key="board.name" class="card">
-            <header class="card-header">
-                <p class="card-header-title">
-                    {{ board.name }}
-                </p>
-                <a href="#" class="card-header-icon" aria-label="more options">
-                    <span class="icon">
-                        <i class="fa fa-angle-down" aria-hidden="true"></i>
-                    </span>
-                </a>
-            </header>
-            <div class="card-content">
-                <div class="content">
-                    {{ board.description }}
+    <main class="section column is-three-quarters">
+        <div class="grid">
+            <div v-for="board in boards" :key="board.name" class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        {{ board.name }}
+                    </p>
+                    <a href="#" class="card-header-icon" aria-label="more options">
+                        <span class="icon">
+                            <i class="fa fa-angle-down" aria-hidden="true"></i>
+                        </span>
+                    </a>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        {{ board.description }}
+                    </div>
                 </div>
+                <footer class="card-footer">
+                    <router-link to="" class="card-footer-item">Star</router-link>
+                    <router-link :to="`~${board.slug}`" class="card-footer-item">Visit</router-link>
+                    <router-link to="" class="card-footer-item">Ditch</router-link>
+                </footer>
             </div>
-            <footer class="card-footer">
-                <router-link to="" class="card-footer-item">Star</router-link>
-                <router-link :to="`~${board.slug}`" class="card-footer-item">Visit</router-link>
-                <router-link to="" class="card-footer-item">Ditch</router-link>
-            </footer>
         </div>
     </main>
 </template>
@@ -47,5 +49,30 @@ export default Vue.component('forum', {
 
 
 <style lang="sass">
+.grid
+    width: 100%
+    display: flex
+    flex-flow: row wrap
 
+.grid .card
+    flex: 1 1 15em
+    max-width: 50%
+    margin: 1em
+
+@supports(display: grid)
+    .grid
+        display: grid
+        grid-template-columns: repeat(auto-fit, minmax(15em, 1fr))
+        grid-gap: 1em
+
+    .grid .card
+        max-width: none
+        margin: 0
+
+.card
+    display: flex
+    flex-direction: column
+
+.card-content
+    flex: 1 1 auto
 </style>
