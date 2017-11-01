cd47d34344f903f38de5d4cb31841058c2ba646d
annaelde <elde.av@gmail.com>
2017-11-01 00:16:00 -0400

    add compose post modal.

---
+++ src/components/parts/Compose.vue
@@ -0,0 +1,66 @@
+<template>
+    <transition name="fade">
+        <div class="modal compose is-active">
+            <div @click="$emit('close')" class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Post a Thread on {{ board.name }}</p>
+                    <button class="delete" aria-label="close" @click="$emit('close')"></button>
+                </header>
+                <section class="modal-card-body">
+                    <transition name="balloon">
+                        <error v-if="error" :response="error"></error>
+                    </transition>
+                    <div class="field">
+                        <label for="title" class="label">Title</label>
+                        <div class="control" :class="{'is-loading' : (state === 'loading')}">
+                            <input class="input" id="title" type="text" value="">
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="content" class="label">Content</label>
+                        <div class="control" :class="{'is-loading' : (state === 'loading')}">
+                            <textarea class="textarea" id="content" type="text" placeholder=""></textarea>
+                        </div>
+                    </div>
+                </section>
+                <footer class="modal-card-foot">
+                    <button @click="post" class="button is-success">
+                        <span class="icon">
+                            <i class="fa fa-bolt"></i>
+                        </span>
+                        <span>Post</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Error from './Error.vue'
+import axios from '../../plugins/axios'
+
+export default Vue.component('compose', {
+    name: 'compose',
+    computed: mapState({
+        state: state => state.thread.machine.state,
+        error: state => state.thread.error
+    }),
+    methods: {
+        post: function() {
+            var title = document.getElementById('title').value
+            var content = document.getElementById('content').value
+            this.$store.dispatch('thread/postThread', { board: this.board.slug, title: title, content: content })
+        }
+    },
+    props: ['board'],
+    watch: {}
+})
+</script>
+
+
+<style lang="sass">
+
+</style>
--- src/components/sidebars/Board.vue
+++ src/components/sidebars/Board.vue
@@ -1,19 +1,35 @@
 <template>
-  <section class="section column">
-      <h1 class="title">{{ board.name }}</h1>
-      <h2 class="subtitle">{{ board.description }}</h2>
-  </section>
+    <section class="section column">
+        <h1 class="title">{{ board.name }}</h1>
+        <h2 class="subtitle">{{ board.description }}</h2>
+        <button class="button is-primary is-medium">
+            <span class="icon">
+                <i class="fa fa-plus"></i>
+            </span>
+            <span @click="showCompose = true">Post a Thread</span>
+        </button>
+        <compose v-if="showCompose" @close="showCompose = false" :board="board"></compose>
+    </section>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Compose from '../parts/Compose.vue'
 
 export default Vue.component('board-sidebar', {
     name: 'board-sidebar',
     computed: mapState({
         board: state => state.board.data,
         error: state => state.board.error
-    })
+    }),
+    data: function(){
+        return {
+            showCompose: false
+        }
+    },
+    methods: {
+
+    }
 })
 </script>
 
