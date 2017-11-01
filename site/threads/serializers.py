ca691cc5b40a8287dd2e136d8c9d904cc5c21f90
annaelde <elde.av@gmail.com>
2017-11-01 00:13:04 -0400

    make author/board read/write

--- site/threads/serializers.py
+++ site/threads/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 
+from boards.models import Board
+from users.models import User
+
 from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    created = serializers.DateTimeField()
-    author = serializers.ReadOnlyField(source='author.username')
-    board = serializers.ReadOnlyField(source='board.name')
+    created = serializers.DateTimeField(read_only=True)
+    author = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
+    board = serializers.SlugRelatedField(slug_field='slug', queryset=Board.objects.all())
 
     class Meta:
         model = Post
         fields = ('title', 'author', 'content',
-                  'created', 'key', 'slug', 'board')
+                  'created', 'board', 'key', 'slug',)
+        read_only_fields = ('key', 'slug',)
