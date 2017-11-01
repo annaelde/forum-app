26f189aaa7d025fc4b27de2d13da15911523e1ab
annaelde <elde.av@gmail.com>
2017-11-01 00:12:46 -0400

    add create view and rename classes to use correct format.

--- site/threads/views.py
+++ site/threads/views.py
@@ -1,5 +1,5 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from utils.mixins import MultipleFieldLookupMixin
 
@@ -7,19 +7,25 @@ from .models import Post
 from .serializers import PostSerializer
 
 
-class thread_list(ListAPIView):
+class ThreadList(ListAPIView):
     serializer_class = PostSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
         board = self.kwargs['board']
         return Post.objects.filter(board__slug=board)
 
 
-class thread_detail(MultipleFieldLookupMixin, RetrieveAPIView):
+class ThreadDetail(MultipleFieldLookupMixin, RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     lookup_fields = ['board__slug', 'key', 'slug']
     lookup_url_kwargs = ['board', 'key', 'thread']
     model = Post
+
+
+class ThreadCreate(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated,)
