a656fa32267f66dbebfe40aa5b594d77d683309e
annaelde <elde.av@gmail.com>
2017-11-02 05:44:39 -0400

    use v-model now for input.

--- src/components/parts/LoginModal.vue
+++ src/components/parts/LoginModal.vue
@@ -1,44 +1,44 @@
 <template>
     <transition name="fade">
-    <div class="modal login is-active">
-        <div @click="$emit('close')" class="modal-background"></div>
-        <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">Log into Forum</p>
-                <button class="delete" aria-label="close" @click="$emit('close')"></button>
-            </header>
-            <section class="modal-card-body">
-                <transition name="balloon">
-                    <error-message v-if="error" :response="error"></error-message>
-                </transition>
-                <div class="field">
-                    <label for="username" class="label">Username</label>
-                    <div class="control has-icons-left" :class="{'is-loading' : (state === 'loading')}">
-                        <input class="input" id="username" type="text" value="">
-                        <span class="icon is-small is-left">
-                        <i class="fa fa-user"></i>
-                        </span>
+        <div class="modal login is-active">
+            <div @click="$emit('close')" class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Log into Forum</p>
+                    <button class="delete" aria-label="close" @click="$emit('close')"></button>
+                </header>
+                <section class="modal-card-body">
+                    <transition name="balloon">
+                        <error-message v-if="error" :response="error"></error-message>
+                    </transition>
+                    <div class="field">
+                        <label for="username" class="label">Username</label>
+                        <div class="control has-icons-left" :class="{'is-loading' : (state === 'loading')}">
+                            <input class="input" id="username" v-model="username" type="text" value="">
+                            <span class="icon is-small is-left">
+                                <i class="fa fa-user"></i>
+                            </span>
+                        </div>
                     </div>
-                </div>
-                <div class="field">
-                    <label for="password" class="label">Password</label>
-                    <div class="control has-icons-left" :class="{'is-loading' : (state === 'loading')}">
-                        <input class="input" id="password" type="password" value="">
-                        <span class="icon is-small is-left">
-                        <i class="fa fa-key"></i>
-                        </span>
+                    <div class="field">
+                        <label for="password" class="label">Password</label>
+                        <div class="control has-icons-left" :class="{'is-loading' : (state === 'loading')}">
+                            <input class="input" id="password" v-model="password" type="password" value="">
+                            <span class="icon is-small is-left">
+                                <i class="fa fa-key"></i>
+                            </span>
+                        </div>
                     </div>
-                </div>
-            </section>
-            <footer class="modal-card-foot">
-                <button @click="login" class="button is-success">Login</button>
-                <div class="register">
-                    <span class="is-hidden-mobile">Don't have an account yet?</span>
-                    <button class="button">Register</button>
-                </div>
-            </footer>
+                </section>
+                <footer class="modal-card-foot">
+                    <button @click="login" class="button is-success">Login</button>
+                    <div class="register">
+                        <span class="is-hidden-mobile">Don't have an account yet?</span>
+                        <button class="button">Register</button>
+                    </div>
+                </footer>
+            </div>
         </div>
-    </div>
     </transition>
 </template>
 
@@ -48,23 +48,29 @@ import ErrorMessage from './ErrorMessage.vue'
 import axios from '../../libs/axios'
 
 export default Vue.component('login-modal', {
+    data: function() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
     computed: mapState({
         auth: state => state.user.token,
         state: state => state.user.machine.state,
         user: state => state.user.data,
         error: state => state.user.error
     }),
-    methods: {
-        login: function() {
-            this.$store.dispatch('user/authenticate', {
-                username: document.getElementById('username').value,
-                password: document.getElementById('password').value
-            })
+    watch: {
+        auth: function() {
+            if (this.auth) this.$emit('close')
         }
     },
-    watch: {
-        auth: function(){
-            if (this.auth) this.$emit('close')
+    methods: {
+        login: function() {
+            this.$store.dispatch('user/authenticate', { 
+                username: this.username, 
+                password: this.password 
+            })
         }
     }
 })
