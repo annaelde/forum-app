462b55e4276207187ea74c243fa62959ec0f589c
annaelde <elde.av@gmail.com>
2017-11-05 10:07:57 -0500

    considering refactoring stores to extend abstract store.

---
+++ src/store/abstract.js
@@ -0,0 +1,19 @@
+// Abstract store to extend
+export const AbstractStore = {
+    state: {
+        data: null,
+        machine: new StateMachine(),
+        error: ''
+    },
+    mutations: {
+        SET_DATA(state, data) {
+            state.data = data
+        },
+        SET_ERROR(state, message) {
+            state.error = message
+        },
+        SET_STATE(state, action) {
+            state.machine.do(action)
+        }
+    }
+}
