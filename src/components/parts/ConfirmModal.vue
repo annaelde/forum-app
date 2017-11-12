b3f91b5c0683ea6d09f52dec5b661a44f63e7f4e
annaelde <elde.av@gmail.com>
2017-11-11 20:25:35 -0500

    add confirm modal component for future integration with vuex.

---
+++ src/components/parts/ConfirmModal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">
+                    {{ title }}
+                </p>
+                <button class="delete" aria-label="close" @click="$emit('cancel')"></button>
+            </header>
+            <section class="modal-card-body">
+                {{ body }}
+            </section>
+            <footer class="modal-card-foot">
+                <button class="button" @click="$emit('confirm')">Confirm</button>
+                <button class="button" @click="$emit('cancel')">Cancel</button>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+export default Vue.component('confirm-modal', {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        body: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
