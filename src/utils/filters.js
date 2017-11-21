c1785dcf3b0271ba6383c8d8b6e2b354bf1836e6
annaelde <elde.av@gmail.com>
2017-11-21 15:33:14 -0500

    add readable date filter.

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -3,3 +3,7 @@ import moment from "moment"
 Vue.filter('timeElapsed', timestamp => {
         return moment(timestamp).fromNow()
 })
+
+Vue.filter('readableDate', timestamp => {
+    return moment(timestamp).calendar()
+})
