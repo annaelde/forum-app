b9fe775481c1cc6a1034dbbe74829133a3442f63
annaelde <elde.av@gmail.com>
2017-11-21 15:34:41 -0500

    begin implementing profile view + sidebar

---
+++ src/components/sidebars/ProfileSidebar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="sidebar">
+        <div class="content">
+            <h1 class="title">{{ user.username }}</h1>
+            <h2 class="subtitle is-5">Joined on {{ user.date_joined | readableDate }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default Vue.component('profile-sidebar', {
+    computed: mapGetters('user', {
+        user: 'GET_USER'
+    })
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/components/views/ProfileView.vue
@@ -0,0 +1,16 @@
+<template>
+    <main class="main">
+
+    </main>
+</template>
+
+<script>
+export default Vue.component('profile-view', {
+
+    
+})
+</script>
+
+<style>
+
+</style>
