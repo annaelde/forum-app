1770160964614e522bd2bdef181541aa2d32b0ff
annaelde <elde.av@gmail.com>
2017-11-24 16:34:47 -0500

    split user serializer into public/private serializer.

--- site/users/serializers.py
+++ site/users/serializers.py
@@ -6,10 +6,18 @@ from threads.models import Post
 from .models import User
 
 
-class UserSerializer(ModelSerializer):
+class PublicUserSerializer(ModelSerializer):
     posts = PrimaryKeyRelatedField(many=True,
                                    queryset=Post.objects.all())
 
     class Meta:
         model=User
-        fields=('username', 'email', 'date_joined', 'last_login', 'posts')
+        fields=('username', 'date_joined', 'posts')
+
+class PrivateUserSerializer(ModelSerializer):
+    posts = PrimaryKeyRelatedField(many=True,
+                                   queryset=Post.objects.all())
+
+    class Meta:
+        model=User
+        fields=('username','email', 'date_joined', 'last_login', 'posts')
