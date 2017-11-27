91611b68718b65cd61cb1d9027d9c54eac7239bb
annaelde <elde.av@gmail.com>
2017-11-27 17:37:25 -0500

    modify request function to have the option of using callbacks instead of mutations so it's usable outside of vuex.

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -17,45 +17,60 @@ export function removeHeader(key) {
 }
 
 /**
- * 
+ *
  * @param {Object} context - Vuex store context
  * @param {string} method - HTTP Method
  * @param {string} url - API URL
  * @param {Object} payload - Payload to send to API
  * @param {Array} mutations - All mutations to commit
- * @param {boolean} root - Whether to commit mutations in global scope 
+ * @param {boolean} root - Whether to commit mutations in global scope
+ * @param {boolean} callback - Callback when function is done
  * @param {boolean} chain - Whether to resolve state, or leave loading
  */
-export async function request({ context, method, url, payload, mutations, root, chain }) {
-    var resolved = true
-    context.commit('SET_STATE', 'request', { root })
+export async function request({ context, method, url, payload, mutations, root = false, callback = null, chain = false }) {
+    let resolved = true
+    let result = null
+
+    if (!callback) {
+        context.commit('SET_STATE', 'request', { root })
+    }
 
     await http[method](url, payload)
         .then(response => {
-            forEach(mutations, mutation => {
-                context.commit(mutation, response.data)
-            })
+            if (callback) {
+                result = response
+            } else {
+                forEach(mutations, mutation => {
+                    context.commit(mutation, response.data)
+                })
+            }
         })
         .catch(error => {
-            context.commit('SET_ERROR', error.response, { root })
-
-            if (chain) {
-                context.commit('SET_STATE', 'handle', { root })
-            } else {
-                context.commit('SET_STATE', 'reject', { root })
+            if (!callback) {
+                context.commit('SET_ERROR', error.response, { root })
+                if (chain) {
+                    context.commit('SET_STATE', 'handle', { root })
+                } else {
+                    context.commit('SET_STATE', 'reject', { root })
+                }
             }
 
             resolved = false
         })
 
-    if (resolved && chain) {
-        context.commit('SET_STATE', 'update', { root })
-    } else if (resolved) {
-        context.commit('SET_STATE', 'resolve', { root })
-    }
+    // If a callback was passed, use it now
+    if (callback) {
+        callback(result)
+    } else {
+        if (resolved && chain) {
+            context.commit('SET_STATE', 'update', { root })
+        } else if (resolved) {
+            context.commit('SET_STATE', 'resolve', { root })
+        }
 
-    // If an error was previously raised, remove it
-    if (resolved && context.getters.GET_ERROR) {
-        context.commit('SET_ERROR', '', { root })
+        // If an error was previously raised, remove it
+        if (resolved && context.getters.GET_ERROR) {
+            context.commit('SET_ERROR', '', { root })
+        }
     }
 }
