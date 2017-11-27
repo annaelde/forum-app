79d942289b79a0b105c78df751a0983d0a0cc02f
annaelde <elde.av@gmail.com>
2017-11-27 14:26:02 -0500

    add thread previews

--- src/components/views/ProfileView.vue
+++ src/components/views/ProfileView.vue
@@ -1,12 +1,17 @@
 <template>
     <main class="main">
-
+        <thread-card v-for="thread in threads" :key="thread.name" :board="thread.board" :thread="thread"></thread-card>
     </main>
 </template>
 
 <script>
-export default Vue.component('profile-view', {
+import { mapGetters } from 'vuex'
+import ThreadCard from '../parts/ThreadCard.vue'
 
+export default Vue.component('profile-view', {
+    computed: mapGetters('user', {
+        threads: 'GET_THREADS'
+    })
     
 })
 </script>
