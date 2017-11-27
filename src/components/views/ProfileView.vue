66ded3c82f42f9dff30764b8b8484660979b196a
annaelde <elde.av@gmail.com>
2017-11-27 17:39:11 -0500

    profile view now gets data by itself instead of using vuex.

--- src/components/views/ProfileView.vue
+++ src/components/views/ProfileView.vue
@@ -1,21 +1,50 @@
 <template>
     <main class="main">
-        <thread-card v-for="thread in threads" :key="thread.name" :board="thread.board" :thread="thread"></thread-card>
+        <thread-card 
+        v-if="threads" 
+        v-for="thread in threads" 
+        :key="thread.name" 
+        :board="thread.board" 
+        :thread="thread"></thread-card>
+        <div v-if="user && !threads">
+            <p class="title">
+                This user hasn't posted any threads.
+            </p>
+        </div>
+        <div v-else>
+            <p class="title">
+                Oh no, 404! This user doesn't exist.
+            </p>
+        </div>
     </main>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { request } from '../../libs/axios'
 import ThreadCard from '../parts/ThreadCard.vue'
 
 export default Vue.component('profile-view', {
-    computed: mapGetters('user', {
-        threads: 'GET_THREADS'
-    })
-    
+    data() {
+        return {
+            user: null
+        }
+    },
+    methods: {
+        setUser: async function() {
+            await request({
+                method: 'get',
+                url: `users/${this.$route.params.username}`,
+                callback: response => { this.user = response ? response.data : null }
+            })
+        }
+    },
+    computed: {
+        threads: function() {
+            return this.user ? this.user.posts : false
+        }
+    },
+    async created() {
+        await this.setUser()
+    }
 })
 </script>
-
-<style>
-
-</style>
