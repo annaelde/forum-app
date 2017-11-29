20cf9e71868045259793d4b8843ddeaf10c80ac5
annaelde <elde.av@gmail.com>
2017-11-28 22:11:03 -0500

    extract ProgressBar component.

---
+++ src/components/parts/ProgressBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="progress-container">
+        <transition name="fade-out" appear>
+            <progress v-show="progress != 100" class="progress is-info is-small is-radiusless" :value="progress" max="100" />
+        </transition>
+    </div>
+</template>
+
+<script>
+export default Vue.component('progress-bar', {
+    props: {
+        progress: {
+            type: Number,
+            required: true
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
