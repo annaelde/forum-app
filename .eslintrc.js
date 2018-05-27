6041232e5389659b2350118b3205401edd30bb44
annaelde <elde.av@gmail.com>
2018-05-27 16:05:04 -0400

    change switch indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,9 @@ module.exports = {
         "Vue": false
     },
     rules: {
-        indent: ['error', 4],
+        indent: ['error', 4, {
+            "SwitchCase": 1
+        }],
         'linebreak-style': ['error', 'windows'],
         quotes: ['error', 'single'],
         semi: ['error', 'never'],
